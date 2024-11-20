bc466c1e4ff76ea0da9c2da22356ddbd231a416c
Rodrigo Castro <rodrigo.castro@kinghost.com.br>
2024-11-20 17:24:22 -0300

    instalando o flutter dot env para variaveis de ambiente do projeto

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:adote_um_pet/src/app_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: '.env');
   runApp(const AppWidget());
 }
