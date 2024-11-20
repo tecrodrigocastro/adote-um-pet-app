9141490df687b0279ea2b986d0badaa5cd03a6f8
Rafael Farias <e.rafaelfarias@gmail.com>
2024-11-20 15:07:08 -0300

    Criação da tela de auth

--- lib/src/app/features/auth/presentation/pages/auth_base_page.dart
+++ lib/src/app/features/auth/presentation/pages/auth_base_page.dart
@@ -1,14 +1,43 @@
+import 'package:adote_um_pet/src/routes.dart';
+import 'package:design_system/design_system.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:go_router/go_router.dart';
 
 class AuthBasePage extends StatelessWidget {
   const AuthBasePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Auth Base Page'),
+    final size = MediaQuery.sizeOf(context);
+    return Scaffold(
+        body: SizedBox(
+      width: size.width,
+      height: size.height,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Image(image: AppImages.logo),
+          const Gap(50),
+          PrimaryButtonDs(
+            title: 'Login',
+            onPressed: () {
+              router.go('/auth/login');
+            },
+          ),
+          const Gap(20),
+          PrimaryButtonDs(
+            title: 'Registrar',
+            onPressed: () {
+              context.go('/auth/register');
+            },
+            backgroundColor: AppColors.secondaryColor,
+          ),
+          const Gap(20),
+          const Text('Powered by Flutterando')
+        ],
       ),
-    );
+    ));
   }
 }
---
+++ lib/src/app/features/auth/presentation/pages/register_page.dart
@@ -0,0 +1,142 @@
+import 'package:adote_um_pet/src/routes.dart';
+import 'package:design_system/design_system.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_masked_text2/flutter_masked_text2.dart';
+import 'package:gap/gap.dart';
+import 'package:go_router/go_router.dart';
+
+class RegisterPage extends StatefulWidget {
+  const RegisterPage({super.key});
+
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  MaskedTextController numberController = MaskedTextController(mask: '(00) 00000-0000');
+  MaskedTextController cepController = MaskedTextController(mask: '00000-000');
+  TextEditingController addressController = TextEditingController();
+  TextEditingController numberHouseController = TextEditingController();
+  TextEditingController complementController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  TextEditingController confirmPasswordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.sizeOf(context);
+    return Scaffold(
+      backgroundColor: AppColors.whiteColor,
+      appBar: AppBar(
+        backgroundColor: AppColors.whiteColor,
+        leading: IconButton(
+          onPressed: () {
+            context.pop();
+          },
+          icon: const Icon(
+            Icons.arrow_back_ios,
+            color: AppColors.primaryColor,
+            size: 30,
+          ),
+        ),
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(17),
+        width: size.width,
+        height: size.height,
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                'Cadastro',
+                style: TextStyle(
+                  color: AppColors.primaryColor,
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const Gap(29),
+              TextInputDs(
+                label: 'nome',
+                controller: nameController,
+                width: size.width,
+              ),
+              const Gap(25),
+              TextInputDs(
+                label: 'e-mail',
+                controller: emailController,
+                textInputType: TextInputType.emailAddress,
+                width: size.width,
+              ),
+              const Gap(25),
+              TextInputDs(
+                width: size.width,
+                label: 'senha',
+                controller: passwordController,
+                isPassword: true,
+              ),
+              const Gap(25),
+              TextInputDs(
+                width: size.width,
+                label: 'confirmar senha',
+                controller: confirmPasswordController,
+                isPassword: true,
+              ),
+              const Gap(25),
+              TextInputDs(
+                label: 'telefone',
+                controller: numberController,
+                textInputType: TextInputType.number,
+                width: size.width,
+              ),
+              const Gap(25),
+              TextInputDs(
+                label: 'cep',
+                controller: cepController,
+                textInputType: TextInputType.number,
+                width: size.width,
+              ),
+              const Gap(25),
+              TextInputDs(
+                label: 'Endereço',
+                controller: addressController,
+                textInputType: TextInputType.number,
+                width: size.width,
+              ),
+              const Gap(25),
+              Row(
+                children: [
+                  TextInputDs(
+                    label: 'Número',
+                    controller: numberHouseController,
+                    textInputType: TextInputType.number,
+                    width: size.width * 0.3,
+                  ),
+                  const Gap(10),
+                  TextInputDs(
+                    label: 'Complemento',
+                    controller: complementController,
+                    textInputType: TextInputType.number,
+                    width: size.width * 0.59,
+                  ),
+                ],
+              ),
+              const Gap(40),
+              Align(
+                alignment: Alignment.center,
+                child: PrimaryButtonDs(
+                  title: 'Cadastrar',
+                  onPressed: () {
+                    router.go('/auth/welcome');
+                  },
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
