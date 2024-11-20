54b050a257be794942c3b28c982297c3235c3088
Rodrigo Castro <rodrigo.castro@kinghost.com.br>
2024-11-20 11:35:06 -0300

    rotas do go router

--- lib/src/routes.dart
+++ lib/src/routes.dart
@@ -1,17 +1,24 @@
+import 'package:adote_um_pet/src/app/features/auth/presentation/pages/auth_base_page.dart';
 import 'package:adote_um_pet/src/app/features/auth/presentation/pages/login_page.dart';
 import 'package:adote_um_pet/src/core/features/onboarding/presentation/pages/onboarding_page.dart';
 import 'package:go_router/go_router.dart';
 
 final router = GoRouter(
-  initialLocation: '/onboarding',
+  initialLocation: '/auth',
   routes: [
     GoRoute(
       path: '/onboarding',
       builder: (context, state) => const OnboardingPage(),
     ),
     GoRoute(
-      path: '/login',
-      builder: (context, state) => const LoginPage(),
+      path: "/auth",
+      builder: (context, state) => const AuthBasePage(),
+      routes: [
+        GoRoute(
+          path: 'login',
+          builder: (context, state) => const LoginPage(),
+        ),
+      ],
     ),
   ],
 );
