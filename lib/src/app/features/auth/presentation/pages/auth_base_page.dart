5bda37795ec102a4d986f1210cc87881bf5aadc4
Rodrigo Castro <rodrigo.castro@kinghost.com.br>
2024-11-20 11:36:04 -0300

    paginas de auth

---
+++ lib/src/app/features/auth/presentation/pages/auth_base_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AuthBasePage extends StatelessWidget {
+  const AuthBasePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Auth Base Page'),
+      ),
+    );
+  }
+}
