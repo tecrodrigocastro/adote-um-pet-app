2daef09707655b62f30d611dbe399f759c16f480
Rodrigo Castro <rodrigo.castro@kinghost.com.br>
2024-11-20 22:49:43 -0300

    assinando os bloc providers

--- lib/src/app_widget.dart
+++ lib/src/app_widget.dart
@@ -1,19 +1,27 @@
+import 'package:adote_um_pet/src/app/features/auth/presentation/bloc/auth_bloc.dart';
+import 'package:adote_um_pet/src/core/DI/dependency_injector.dart';
 import 'package:adote_um_pet/src/routes.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class AppWidget extends StatelessWidget {
   const AppWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      debugShowCheckedModeBanner: false,
-      title: 'Adote um Pet',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => injector<AuthBloc>()),
+      ],
+      child: MaterialApp.router(
+        debugShowCheckedModeBanner: false,
+        title: 'Adote um Pet',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        routerConfig: router,
       ),
-      routerConfig: router,
     );
   }
 }
