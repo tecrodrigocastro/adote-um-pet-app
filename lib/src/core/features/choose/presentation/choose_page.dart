e46fe3bd7445b0b45357e856de7e1574932e4f43
Adryanne Kelly <adryannekelly123@gmail.com>
2024-11-20 23:00:45 -0300

    feat: adicionando imagens e pagina de escolha de doacao e adocao

---
+++ lib/src/core/features/choose/presentation/choose_page.dart
@@ -0,0 +1,65 @@
+import 'package:adote_um_pet/src/routes.dart';
+import 'package:design_system/design_system.dart';
+import 'package:flutter/material.dart';
+
+class ChoosePage extends StatelessWidget {
+  const ChoosePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.sizeOf(context);
+    final theme = Theme.of(context);
+    return Scaffold(
+      body: Column(
+        children: [
+          InkWell(
+            onTap: () {},
+            child: Container(
+              height: size.height * 0.5,
+              width: size.width,
+              alignment: AlignmentDirectional.bottomStart,
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AppImages.dogChoose,
+                  fit: BoxFit.cover,
+                  colorFilter: ColorFilter.mode(
+                    AppColors.secondaryColor,
+                    BlendMode.darken,
+                  ),
+                ),
+              ),
+              child: Text(
+                'adotar',
+                style: theme.textTheme.displayLarge,
+              ),
+            ),
+          ),
+          InkWell(
+            onTap: () {
+              router.go('/donate-info');
+            },
+            child: Container(
+              height: size.height * 0.5,
+              width: size.width,
+              alignment: AlignmentDirectional.bottomStart,
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AppImages.catChoose,
+                  fit: BoxFit.cover,
+                  colorFilter: ColorFilter.mode(
+                    AppColors.secondaryColor,
+                    BlendMode.darken,
+                  ),
+                ),
+              ),
+              child: Text(
+                'doar',
+                style: theme.textTheme.displayLarge,
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
