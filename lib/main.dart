4b832557c250bf5ea5eaa79491e99884896b1dc6
Rodrigo Castro <rodrigo.castro@kinghost.com.br>
2024-11-20 22:49:21 -0300

    inicializando a injecao de dependencia

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:adote_um_pet/src/app_widget.dart';
+import 'package:adote_um_pet/src/core/DI/dependency_injector.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: '.env');
+  setupDependencyInjector();
   runApp(const AppWidget());
 }
