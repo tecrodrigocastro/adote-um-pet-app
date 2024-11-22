7aca256aac05173268f064d801a300bcfdf20a55
Lorenzo DZ <lorenzo.dz@hotmail.com>
2024-11-22 16:28:49 -0300

    feat(design_system/image): Add images for pet type buttons

--- design_system/lib/src/image/app_image.dart
+++ design_system/lib/src/image/app_image.dart
@@ -2,7 +2,32 @@ import 'package:flutter/material.dart';
 
 class AppImages {
   static const String packageName = 'design_system';
-  static const logo = AssetImage('assets/images/logo.png', package: packageName);
-  static const dogChoose = AssetImage('assets/images/dog-choose.png', package: packageName);
-  static const catChoose = AssetImage('assets/images/cat-choose.png', package: packageName);
+  static const logo = AssetImage(
+    'assets/images/logo.png',
+    package: packageName,
+  );
+  static const dogChoose = AssetImage(
+    'assets/images/dog-choose.png',
+    package: packageName,
+  );
+  static const catChoose = AssetImage(
+    'assets/images/cat-choose.png',
+    package: packageName,
+  );
+  static const dogChooseButton = AssetImage(
+    'assets/images/dog-button.png',
+    package: packageName,
+  );
+  static const catChooseButton = AssetImage(
+    'assets/images/cat-button.png',
+    package: packageName,
+  );
+  static const birdChooseButton = AssetImage(
+    'assets/images/bird-button.png',
+    package: packageName,
+  );
+  static const coelhoChooseButton = AssetImage(
+    'assets/images/coelho-button.png',
+    package: packageName,
+  );
 }
