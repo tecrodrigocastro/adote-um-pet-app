0b778617ffcf22a57d8f3fc888ad0bf48bca4e0b
Rodrigo Castro <rodrigo.castro@kinghost.com.br>
2024-11-22 23:20:05 -0300

    chamando bloc de registro e adicionando reatividade

--- lib/src/app/features/auth/presentation/pages/register_page.dart
+++ lib/src/app/features/auth/presentation/pages/register_page.dart
@@ -1,10 +1,14 @@
 import 'package:design_system/design_system.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_masked_text2/flutter_masked_text2.dart';
 import 'package:gap/gap.dart';
+import 'package:get_it/get_it.dart';
 import 'package:go_router/go_router.dart';
 
+import '../../../../../core/utils/show_snack_bar.dart';
 import '../../../../../routes.dart';
+import '../bloc/auth_bloc.dart';
 
 class RegisterPage extends StatefulWidget {
   const RegisterPage({super.key});
@@ -25,10 +29,13 @@ class _RegisterPageState extends State<RegisterPage> {
   final _passwordController = TextEditingController();
   final _confirmPasswordController = TextEditingController();
 
+  final formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.sizeOf(context);
     final theme = Theme.of(context);
+    final authBloc = GetIt.I.get<AuthBloc>();
     return Scaffold(
       appBar: AppBar(
         leading: IconButton(
@@ -45,92 +52,141 @@ class _RegisterPageState extends State<RegisterPage> {
         width: size.width,
         height: size.height,
         child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'Cadastro',
-                style: theme.textTheme.displaySmall,
-              ),
-              const Gap(29),
-              TextInputDs(
-                label: 'nome',
-                controller: _nameController,
-                width: size.width,
-              ),
-              const Gap(25),
-              TextInputDs(
-                label: 'e-mail',
-                controller: _emailController,
-                textInputType: TextInputType.emailAddress,
-                width: size.width,
-              ),
-              const Gap(25),
-              TextInputDs(
-                width: size.width,
-                label: 'senha',
-                controller: _passwordController,
-                isPassword: true,
-              ),
-              const Gap(25),
-              TextInputDs(
-                width: size.width,
-                label: 'confirmar senha',
-                controller: _confirmPasswordController,
-                isPassword: true,
-              ),
-              const Gap(25),
-              TextInputDs(
-                label: 'telefone',
-                controller: numberController,
-                textInputType: TextInputType.number,
-                width: size.width,
-              ),
-              const Gap(25),
-              TextInputDs(
-                label: 'cep',
-                controller: cepController,
-                textInputType: TextInputType.number,
-                width: size.width,
-              ),
-              const Gap(25),
-              TextInputDs(
-                label: 'Endereço',
-                controller: _addressController,
-                textInputType: TextInputType.number,
-                width: size.width,
-              ),
-              const Gap(25),
-              Row(
-                children: [
-                  TextInputDs(
-                    label: 'Número',
-                    controller: _numberHouseController,
-                    textInputType: TextInputType.number,
-                    width: size.width * 0.3,
-                  ),
-                  const Gap(10),
-                  Flexible(
-                    child: TextInputDs(
-                      label: 'Complemento',
-                      controller: _complementController,
-                      textInputType: TextInputType.number,
-                      width: size.width * 0.59,
-                    ),
-                  ),
-                ],
-              ),
-              const Gap(40),
-              Align(
-                alignment: Alignment.center,
-                child: PrimaryButtonDs(
-                  title: 'Cadastrar',
-                  onPressed: () {
-                    router.go('/auth/welcome');
-                  },
+          child: Form(
+            key: formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Cadastro',
+                  style: theme.textTheme.displaySmall,
                 ),
-              )
-            ],
+                const Gap(29),
+                TextInputDs(
+                  label: 'nome',
+                  controller: _nameController,
+                  width: size.width,
+                ),
+                const Gap(25),
+                TextInputDs(
+                  label: 'e-mail',
+                  controller: _emailController,
+                  textInputType: TextInputType.emailAddress,
+                  width: size.width,
+                ),
+                const Gap(25),
+                TextInputDs(
+                  width: size.width,
+                  label: 'senha',
+                  controller: _passwordController,
+                  isPassword: true,
+                ),
+                const Gap(25),
+                TextInputDs(
+                  width: size.width,
+                  label: 'confirmar senha',
+                  controller: _confirmPasswordController,
+                  isPassword: true,
+                ),
+                const Gap(25),
+                TextInputDs(
+                  label: 'telefone',
+                  controller: numberController,
+                  textInputType: TextInputType.number,
+                  width: size.width,
+                ),
+                const Gap(25),
+                TextInputDs(
+                  label: 'cep',
+                  controller: cepController,
+                  textInputType: TextInputType.number,
+                  width: size.width,
+                ),
+                const Gap(25),
+                TextInputDs(
+                  label: 'Endereço',
+                  controller: _addressController,
+                  textInputType: TextInputType.number,
+                  width: size.width,
+                ),
+                const Gap(25),
+                Row(
+                  children: [
+                    TextInputDs(
+                      label: 'Número',
+                      controller: _numberHouseController,
+                      textInputType: TextInputType.number,
+                      width: size.width * 0.3,
+                    ),
+                    const Gap(10),
+                    Flexible(
+                      child: TextInputDs(
+                        label: 'Complemento',
+                        controller: _complementController,
+                        textInputType: TextInputType.number,
+                        width: size.width * 0.59,
+                      ),
+                    ),
+                  ],
+                ),
+                const Gap(40),
+                Align(
+                  alignment: Alignment.center,
+                  child: BlocConsumer<AuthBloc, AuthState>(
+                    listener: (context, state) {
+                      if (state is SignUpAuthSuccess) {
+                        showMessageSnackBar(
+                          context,
+                          state.data.message,
+                          icon: Icons.check,
+                          iconColor: AppColors.whiteColor,
+                          color: AppColors.secondaryColor,
+                        );
+                        formKey.currentState!.reset();
+
+                        router.go('/auth/welcome');
+                      }
+                      if (state is SignUpAuthFailure) {
+                        showMessageSnackBar(
+                          context,
+                          state.message,
+                          icon: Icons.error,
+                          iconColor: AppColors.whiteColor,
+                          color: AppColors.primaryColor,
+                        );
+                      }
+                    },
+                    builder: (context, state) {
+                      if (state is SignUpAuthLoading) {
+                        return const Center(
+                          child: CircularProgressIndicator(),
+                        );
+                      }
+                      return PrimaryButtonDs(
+                        title: 'Cadastrar',
+                        onPressed: () {
+                          if (formKey.currentState!.validate()) {
+                            authBloc.add(
+                              SignUpAuthEvent(
+                                name: _nameController.text.trim(),
+                                email: _emailController.text.trim(),
+                                password: _passwordController.text.trim(),
+                                phone: numberController.text.trim(),
+                                zipCode: cepController.text.trim(),
+                                address: _addressController.text.trim(),
+                                numberHouse: int.parse(
+                                    _numberHouseController.text.trim()),
+                                complement: _complementController.text.trim(),
+                              ),
+                            );
+                          }
+                        },
+                      );
+                    },
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
