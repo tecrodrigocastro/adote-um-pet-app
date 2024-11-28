dbc2af1b4f563a208862326a89339c5d50350e63
Wellington Santos <wellington.santos@genial.com.vc>
2024-11-27 21:41:17 -0300

    refactor: IRestClient

--- lib/src/app/features/auth/data/datasources/auth_remote_datasource.dart
+++ lib/src/app/features/auth/data/datasources/auth_remote_datasource.dart
@@ -1,12 +1,11 @@
 import '../../../../../core/client_http/client_http.dart';
-import '../../../../../core/client_http/dio/rest_client_dio_impl.dart';
 import '../../../../../core/client_http/rest_client_multipart.dart';
 
 class AuthRemoteDatasource implements IRestClient {
-  final RestClientDioImpl _restClientDioImpl;
+  final IRestClient _restClientDioImpl;
 
   AuthRemoteDatasource({
-    required RestClientDioImpl restClientDioImpl,
+    required IRestClient restClientDioImpl,
   }) : _restClientDioImpl = restClientDioImpl;
 
   @override
