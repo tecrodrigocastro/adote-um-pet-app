beef27cd3aeac440a5e5f5a6e28e55a1b3325a15
Rodrigo Castro <rodrigo.castro@kinghost.com.br>
2024-12-28 12:38:04 -0300

    refactor: refatorando a welcome page

--- design_system/lib/src/icons/app_icons.dart
+++ design_system/lib/src/icons/app_icons.dart
@@ -2,9 +2,16 @@ import 'package:flutter/material.dart';
 
 class AppIcons {
   static const String packageName = 'design_system';
-  static const adoptDrawer = AssetImage('assets/icons/adopt-drawer.png', package: packageName);
-  static const donateDrawer = AssetImage('assets/icons/donate-drawer.png', package: packageName);
-  static const messagesDrawer = AssetImage('assets/icons/messages-drawer.png', package: packageName);
-  static const myAccountDrawer = AssetImage('assets/icons/my-account-drawer.png', package: packageName);
-  static const petinhaDrawer = AssetImage('assets/icons/petinha-drawer.png', package: packageName);
+  static const adoptDrawer =
+      AssetImage('assets/icons/adopt-drawer.png', package: packageName);
+  static const donateDrawer =
+      AssetImage('assets/icons/donate-drawer.png', package: packageName);
+  static const messagesDrawer =
+      AssetImage('assets/icons/messages-drawer.png', package: packageName);
+  static const myAccountDrawer =
+      AssetImage('assets/icons/my-account-drawer.png', package: packageName);
+  static const petinhaDrawer =
+      AssetImage('assets/icons/petinha-drawer.png', package: packageName);
+  static const uploadIcon =
+      AssetImage('assets/icons/upload_icon.png', package: packageName);
 }
