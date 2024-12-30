4c941d203c9d74767dd352a399b5d2cfcdf68dff
Gabriel Merenfeld <gabrielmerenfeld@gmail.com>
2024-12-30 15:53:22 -0300

    feat: criacao message_page

---
+++ lib/src/app/features/message/presentation/widgets/profile_photo.dart
@@ -0,0 +1,55 @@
+import 'package:design_system/design_system.dart';
+import 'package:flutter/material.dart';
+
+class ProfilePhoto extends StatelessWidget {
+  //
+  const ProfilePhoto({
+    super.key,
+    required this.isOnline,
+    required this.image,
+  });
+
+  final bool isOnline;
+  final String image;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Stack(
+          clipBehavior: Clip.none,
+          children: [
+            Container(
+              height: 64,
+              width: 64,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: AppColors.blackColor,
+                image: DecorationImage(
+                  image: NetworkImage(image),
+                ),
+              ),
+            ),
+            if (isOnline)
+              Positioned(
+                top: 42,
+                left: 47,
+                child: Container(
+                  height: 22,
+                  width: 22,
+                  decoration: BoxDecoration(
+                    color: AppColors.green,
+                    shape: BoxShape.circle,
+                    border: Border.all(
+                      color: Colors.white,
+                      width: 2,
+                    ),
+                  ),
+                ),
+              ),
+          ],
+        ),
+      ],
+    );
+  }
+}
