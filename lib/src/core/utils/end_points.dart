6c217d755a461ab65f7c52e9cc80d0202965a4b2
Rodrigo Castro <tec.rodrigocastro@gmail.com>
2025-09-14 12:24:17 -0300

    ajustando endpoints

--- lib/src/core/utils/end_points.dart
+++ lib/src/core/utils/end_points.dart
@@ -2,7 +2,10 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class EndPoints {
   static final String baseUrl = dotenv.env['BASE_URL'] ?? '';
+  static final String baseUrlDev = dotenv.env['BASE_URL_DEV'] ?? '';
   static const String prefix = '/api';
+
+  // Auth
   static const String login = '$prefix/auth/login';
   static const String register = '$prefix/auth/register';
   static String updatePhotoUser<T>(T userId) {
